7392c4c8c1dcfdc6aa91aaee05ada27606e72b4b
채지인 <known-person@reverselab.co.kr>
2021-08-29 15:53:03 +0900

    Initial commit

---
+++ src/main/java/com/jinnyjinnyjinjin/linkmart/LinkMartApplication.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.linkmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkMartApplication.class, args);
+	}
+
+}
